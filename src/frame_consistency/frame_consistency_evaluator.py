8de9c7f29e1a8e2c27751e73276c527bf0213506
Jakub Tomaszewski <57564656+JakubTomaszewski@users.noreply.github.com>
2024-05-22 00:44:26 +0200

    Clean some

--- src/frame_consistency/frame_consistency_evaluator.py
+++ src/frame_consistency/frame_consistency_evaluator.py
@@ -21,26 +21,3 @@ class FrameConsistencyEvaluator:
         if debug:
             print("Object count:", object_count)
         return object_count
-
-
-# class VideoConsistencyEvaluator:
-#     def __init__(self, video_captoning_config: dict,
-#                  sentence_similarity_config: dict,
-#                  device: str = "cuda") -> None:
-#         self.video_captioner = VideoCaptioner(**video_captoning_config, device=device)
-#         self.prompt_similarity_evaluator = SentenceSimilarityCalculator(**sentence_similarity_config, device=device)
-
-#     def evaluate(self, prompt: str, frames: torch.Tensor, debug: bool = False) -> float:
-#         """Evaluate the consistency between a video and a prompt.
-
-#         Args:
-#             prompt (str): text prompt
-#             frames (torch.Tensor): tensor containing the frames of the video
-
-#         Returns:
-#             float: similarity between the generated video caption and the text prompt
-#         """
-#         caption = self.video_captioner.generate_caption(frames)
-#         if debug:
-#             print("Generated caption:", caption)
-#         return self.prompt_similarity_evaluator.evaluate(caption, prompt)
