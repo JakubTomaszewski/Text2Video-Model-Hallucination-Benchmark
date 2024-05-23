388f981ac3d9dd9639578a4a7d252ac20345ffd7
Angel Bujalance Gómez <58339896+angelbujalance@users.noreply.github.com>
2024-05-23 12:19:22 +0200

    object count updated

--- src/frame_consistency/frame_consistency_evaluator.py
+++ src/frame_consistency/frame_consistency_evaluator.py
@@ -1,23 +1,84 @@
 import torch
-from .object_count import ObjectCounter
+from object_count import ObjectCounter
+import json
 
 
 class FrameConsistencyEvaluator:
-    def __init__(self, object_counter_config: dict,
+    def __init__(self, object_counter_config: dict, prompts_path: str,
                  device: str = "cuda") -> None:
         self.object_counter = ObjectCounter(**object_counter_config, device=device)
 
-    def evaluate(self, prompt: str, frames: torch.Tensor, debug: bool = False) -> float:
+        with open(prompts_path) as json_file:
+            self.prompts_data = json.load(json_file)
+
+    def get_objects(self, prompt: str):
+        """
+
+        Args:
+            prompt (str): prompt to generate the video
+        Returns:
+            dict: number of objects in the video prompt
+        """
+
+        for prompt_data in self.prompts_data['prompts']:
+            if prompt_data["sentence"] == prompt:
+                return prompt_data["object"]
+
+    def evaluate(self, prompt: str, frames: torch.Tensor, debug: bool = False) -> tuple[float, float]:
         """Evaluate the consistency between video frames and a prompt.
 
         Args:
             prompt (str): text prompt
             frames (torch.Tensor): tensor containing the frames of the video
+            debug (bool): prints the object counts to debug and check the evaluation score
 
         Returns:
-            float: similarity between the generated video caption and the text prompt
+            tuple[float, float]: similarity between the generated video caption and the text prompt.
+            frame_object_score corresponds to the similarity between the object instances in the video
+            and the prompt.
+            frame_count_score: corresponds to the similarity between the number of object instances in the video
+            and the prompt.
         """
-        object_count = self.object_counter.count_objects(frames)
+
+        # it supposes that the number of frames is in the first dimension
+        n_frames = frames.shape[0]
+
+        # counts the object in each frame and stores it in a dictionary
+        frames_dict = {}
+        for i_frame in range(n_frames):
+            object_count = self.object_counter.count_objects(image=frames[i_frame])
+            frames_dict[f"frame_{i_frame + 1}"] = object_count
+
         if debug:
-            print("Object count:", object_count)
-        return object_count
+            print("Object count:", frames_dict)
+
+        prompted_objects = self.get_objects(prompt=prompt)
+        try:
+            n_objects = len(prompted_objects)
+        except TypeError:
+            return 0.0, 0.0
+
+        frame_object_score = 0
+        frame_count_score = 0
+        for frame in frames_dict:
+            object_score = 0
+            count_score = 0
+
+            for prompt_object in prompted_objects.keys():
+                for frame_object in frames_dict[frame]:
+                    if prompt_object == frame_object:
+                        object_score += 1
+                        if prompted_objects[prompt_object] == frames_dict[frame][frame_object]:
+                            count_score += 1
+                        else:
+                            continue
+                    else:
+                        continue
+            object_score /= n_objects
+            count_score /= n_objects
+            frame_object_score += object_score
+            frame_count_score += count_score
+
+        frame_object_score /= n_frames
+        frame_count_score /= n_frames
+        return frame_object_score, frame_count_score
--- src/frame_consistency/object_count/models.py
+++ src/frame_consistency/object_count/models.py
@@ -1,17 +1,50 @@
 import torch
+from transformers import DetrImageProcessor, DetrForObjectDetection
 
 
 class ObjectCounter:
-    def __init__(self, device: str="cuda", **kwargs) -> None:
-        pass
+    def __init__(self, processor: DetrImageProcessor, model: DetrForObjectDetection, device: str="cuda", **kwargs) -> None:
+        """Count the number of objects of the same instance in a single image
 
-    def count_objects(self, image: torch.Tensor) -> int:
-        """Count the number of objects in a single image
+        Args:
+            processor (DetrImageProcessor): prepares the images for the object detector
+            model (DetrForObjectDetection): a convolutional backbone that detects objects in an image
+            device (str): the device used to run the model
+        """
+
+        self.model = model
+        self.processor = processor
+
+    def count_objects(self, image: torch.Tensor) -> dict:
+        """Count the number of objects of the same instance in a single image
 
         Args:
             image (torch.Tensor): image tensor
 
         Returns:
-            int: number of objects in the image
+            dict: number of objects in the image for every class
         """
-        return ...
+
+        # Processes and detects the objects in the image
+        inputs = self.processor(images=image, return_tensors="pt")
+        outputs = self.model(**inputs)
+        frame_height, frame_width, _ = image.shape
+
+        # saves the results
+        target_sizes = torch.tensor([[frame_width, frame_height]])
+        results = self.processor.post_process_object_detection(outputs, target_sizes=target_sizes, threshold=0.9)[0]
+
+        # dictionary that counts the appearance of every object in an image
+        count_objects_image = {}
+        for label in results["labels"]:
+            label_name = self.model.config.id2label[label.item()]
+
+            # if the object is not already detected, creates the key
+            if label_name not in count_objects_image:
+                count_objects_image[label_name] = 1
+
+            # if the key already exists add one to the count
+            else:
+                count_objects_image[label_name] += 1
+
+        return count_objects_image
